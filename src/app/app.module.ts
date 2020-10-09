9099a9c81d338cc1e397ad55952d5999b76d1eb4
Nestor Cortina <junior110120@gmail.com>
2020-10-08 22:28:20 -0500

    configurando bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './views/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContacusComponent } from './views/contacus/contacus.component';
+import { AboutusComponent } from './views/aboutus/aboutus.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContacusComponent,
+    AboutusComponent
   ],
   imports: [
     BrowserModule,
